19e53e543f595093dda43ab6212662d09c2c8d76
Nathan Silva Santos <nathan@w16.com.br>
2023-03-14 01:27:03 -0300

    initial commit

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[ show update destroy ]
+
+  def index
+    @users = User.order("id")
+    render json: @users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+end
